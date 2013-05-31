ab8fddffd4ea7976523f2962442f8c8ff73df67b
Avi Flax <avi@aviflax.com>
2013-05-30 22:30:10 -0400

    Moved DB-related funcs to their own namespace Much cleaner

--- src/r2_api/server/core.clj
+++ src/r2_api/server/core.clj
@@ -1,28 +1,11 @@
 (ns r2-api.server.core
   (:require [r2-api.server.templates :as t]
+            [r2-api.server.db :as db]
             [compojure.core :as c :refer [GET PUT POST DELETE]]
-            [com.ashafa.clutch :as couch]
             [compojure.handler :as ch]
             [ring.adapter.jetty :as ra]))
 
 (def context {:server-name "Avi’s R2"})
-
-(def db (couch/couch "avis-r2"))
-
-(couch/create! db)
-
-(defn get-groups [db]
-  (map #(hash-map :_id (:id %) :name (:value %))
-       (couch/get-view db "api" :groups)))
-
-(defn get-doc
-  "Just for convenience as it’s shorter."
-  [db id]
-  (couch/get-document db id))
-
-(defn get-topics [db group-id]
-  (map #(hash-map :_id (:id %) :name (:value %))
-       (couch/get-view db "api" :topics {:key group-id})))
 
 (c/defroutes server
   (GET "/"
@@ -31,31 +14,31 @@
 
   (GET "/groups"
     []
-    (t/groups context (get-groups db)))
+    (t/groups context (db/get-groups)))
 
   (POST "/groups"
     [name]
-    (couch/assoc! db (str (java.util.UUID/randomUUID)) {:type "group" :name name})
-    (t/groups context (get-groups db)))
+    (db/new-doc! {:type "group" :name name})
+    (t/groups context (db/get-groups)))
 
   (GET "/groups/:group-id"
     {params :params}
-      (t/a-group (merge context params) (get-doc db (:group-id params))))
+      (t/a-group (merge context params) (db/get-doc (:group-id params))))
 
   (GET "/groups/:group-id/topics"
     {params :params}
     (t/topics (merge context params)
-              (couch/get-document db (:group-id params))
-              (get-topics db (:group-id params))))
+              (db/get-doc (:group-id params))
+              (db/get-topics (:group-id params))))
 
   (POST "/groups/:group-id/topics"
     [group-id name]
-    (couch/assoc! db (str (java.util.UUID/randomUUID)) {:type "topic" :name name :group {:id group-id}})
-    (t/topics context (get-doc db group-id) (get-topics db group-id)))
+    (db/new-doc! {:type "topic" :name name :group {:id group-id}})
+    (t/topics context (db/get-doc group-id) (db/get-topics group-id)))
 
   (GET "/groups/:group-id/topics/:topic-id"
     [group-id topic-id]
-    (t/a-topic context (get-doc db group-id) (get-doc db topic-id)))
+    (t/a-topic context (db/get-doc group-id) (db/get-doc topic-id)))
 
   (GET "/groups/:group-id/topics/:topic-id/messages"
     {params :params}
---
+++ src/r2_api/server/db.clj
@@ -0,0 +1,23 @@
+(ns r2-api.server.db
+  (:require [com.ashafa.clutch :as couch]))
+
+(def ^:private db (couch/couch "avis-r2"))
+
+(couch/create! db)
+
+(defn get-doc
+  "Just for convenience as it’s shorter."
+  [id]
+  (couch/get-document db id))
+
+(defn new-doc!
+  [content]
+  (couch/assoc! db (str (java.util.UUID/randomUUID)) content))
+
+(defn get-groups []
+  (map #(hash-map :_id (:id %) :name (:value %))
+       (couch/get-view db "api" :groups)))
+
+(defn get-topics [group-id]
+  (map #(hash-map :_id (:id %) :name (:value %))
+       (couch/get-view db "api" :topics {:key group-id})))
