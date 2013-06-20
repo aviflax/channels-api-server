aviflax/channels-api-server

commit 160ce162a2f37aad880b478836df69f6a8ad5658

Each resource lib should be responsible for generating its own URI

src/r2_api/server/resources/discussions.clj
@@ -16,6 +16,8 @@
 
 (def acceptable-types #{"application/json" "text/html"})
 
+(defn uri [group-id] (str "/groups/" group-id "/discussions"))
+
 (h/deftemplate html-template "templates/discussions.html"
   [context group discussions]
   [:html h/text-node] (h/replace-vars (combine context group))

src/r2_api/server/resources/a_group.clj
@@ -1,5 +1,6 @@
 (ns r2-api.server.resources.a-group
   (:require [r2-api.server.util :refer [combine doc-to-json error-response select-accept-type]]
+            [r2-api.server.resources.discussions :as discussions]
             [compojure.core :refer [GET routes]]
             [net.cgrand.enlive-html :as h]
             [r2-api.server.db :as db]))
@@ -11,7 +12,7 @@
 (h/deftemplate html-template "templates/a_group.html"
   [context group]
   [:html h/text-node] (h/replace-vars (combine context group))
-  [:a#discussions] (h/set-attr :href (str "/groups/" (:_id group) "/discussions")))
+  [:a#discussions] (h/set-attr :href (discussions/uri (:_id group))))
 
 (defn represent [accept-header group-id context]
   (case (select-accept-type acceptable-types accept-header)

commit 3856511d0e9dd8c4751841c0b381dbc79241ba48

Fixed a name conflict bug introduced in 160ce162a2

src/r2_api/server/resources/discussions.clj
@@ -1,5 +1,5 @@
 (ns r2-api.server.resources.discussions
-  (:require [r2-api.server.resources.a-discussion :refer [uri]]
+  (:require [r2-api.server.resources.a-discussion :as a-discussion]
             [r2-api.server.util :refer [acceptable? attr-append combine doc-for-json error-response pretty-json select-accept-type type-supported?]]
             [compojure.core :refer [GET POST routes]]
             [net.cgrand.enlive-html :as h]
@@ -8,7 +8,7 @@
             [clojure.pprint :refer :all]))
 
 (defn to-json [group discussions]
-  (-> {:discussions (map #(-> (assoc % :href (uri (get-in % [:group :id]) (:_id %)), :id (:_id %))
+  (-> {:discussions (map #(-> (assoc % :href (a-discussion/uri (get-in % [:group :id]) (:_id %))
                               (dissoc ,,, :_id :_rev :type :group))
                          discussions)}
       (assoc ,,, :group (doc-for-json group))
@@ -23,7 +23,7 @@
   [:html h/text-node] (h/replace-vars (combine context group))
   [:ul#discussions :li] (h/clone-for [discussion discussions]
                      [:a] (h/do->
-                            (h/set-attr :href (uri (:_id group) (:_id discussion)))
+                            (h/set-attr :href (a-discussion/uri (:_id group) (:_id discussion)))
                             (h/content (:name discussion))))
   [:a#group] (attr-append :href str (:_id group))
   [:input#group-id] (h/set-attr :value (:_id group)))
@@ -67,4 +67,4 @@
               (db/new-doc! (db/create-message-doc group-id discussion-id (:body params))))
             (-> (represent (get headers "accept") group-id context)
                 (assoc ,,, :status 201)
-                (assoc-in ,,, [:headers "Location"] (uri group-id discussion-id)))))))))
+                (assoc-in ,,, [:headers "Location"] (a-discussion/uri group-id discussion-id)))))))))